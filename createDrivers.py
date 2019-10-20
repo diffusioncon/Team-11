f7701f4a03d4290ebc15647b049c88d51ee2edd7
tgesior <tomasz.gesior@gmail.com>
2019-10-20 14:00:18 +0200

    Final push

--- createDrivers.py
+++ createDrivers.py
@@ -4,18 +4,18 @@ import requests
 # Change the endpoint as you need
 endpoint_url = 'http://localhost:5002/Garages/'
 
+import json
+headers = {"Content-Type": "application/json" ,
+           "Accept": 'application/json'}
+
 with open('ParkingLotPreferences.json') as json_file:
     drivers = json.load(json_file)
 
-headers = {"accept": "application/json",
-           "Content-Type": "application/json",}
-import json
 for driver in drivers['drivers']:
-    print(driver)
-    res = requests.post(endpoint_url,
-                        headers=headers,
-                        data=json.dumps(driver),
-                        timeout=3)
-    print(f'Error during update {driver}.\n')
-    print(res.content)
+    try:
+        res = requests.post(endpoint_url, data=json.dumps(driver), headers=headers,timeout=3)
+    except Exception as e:
+        print(e)
+        print(f'Error during update {driver}.\n')
+
 print('End of Script')
